6dded1a397c4c910711ed52db6900d1b2a8135b0
Dwiko Dany Rananta <dwiko.dev@gmail.com>
2025-07-17 22:16:55 +0700

    redirect register and subscribe

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Plan;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
-class SubscribeController extends Controller
+class SubscribeController extends Controller implements HasMiddleware
 {
+
+    public static function middleware(): array
+    {
+        return [
+            'auth',
+        ];
+    }
     public function showPlans() {
         $plans = Plan::all();
 
